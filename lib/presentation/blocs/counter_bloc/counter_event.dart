8318615bb231779e234b2e14301681eadfd62923
SantaCruzAndres <andres123nube@gmail.com>
2024-05-22 10:04:33 -0300

    First bloc crated

---
+++ lib/presentation/blocs/counter_bloc/counter_event.dart
@@ -0,0 +1,15 @@
+part of 'counter_bloc.dart';
+
+sealed class CounterEvent extends Equatable {
+  const CounterEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class CounterIncreased extends CounterEvent {
+  final int value;
+
+  const CounterIncreased(this.value);
+
+}
--- lib/presentation/screens/bloc_counter_screen.dart
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_bloc/counter_bloc.dart';
 
 class BlocCounterScreen extends StatelessWidget {
   const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) =>CounterBloc(),
+      child: const BlocCounterView(),
+    );
+  }
+}
+
+class BlocCounterView extends StatelessWidget {
+  const BlocCounterView({
+    super.key,
+  });
+
+  void increaseCounterBy(BuildContext context, [int value = 1]) {
+    // context.read<CounterBloc>().add(CounterIncreased(value));
+BlocProvider.of<CounterBloc>(context).add(CounterIncreased(value));
+  }// Se utiliza BlocProvider.of() para acceder al Bloc de forma organizada, segura y eficiente, siguiendo las mejores prácticas de inyección de dependencias y gestión del ciclo de vida.
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,31 +34,41 @@ class BlocCounterScreen extends StatelessWidget {
           IconButton(onPressed: (){}, icon: const Icon(Icons.restart_alt_outlined)),
         ],
         ),
-      body: const Center(
-        child: Text('Counter valute: xxxx'),
+      body:  Center(
+        child: context.select((CounterBloc counterBloc) => Text('Counter value: ${counterBloc.state.counter}'),),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
             heroTag: '1',
-            onPressed: (){},
+            onPressed: (){
+              increaseCounterBy(context, 3);
+
+            },
             child: const Text('+3'),
             ),
             const SizedBox(height: 10,),
             FloatingActionButton(
               heroTag: '2',
-            onPressed: (){},
+            onPressed: (){
+              increaseCounterBy(context, 2);
+              
+
+
+            },
             child: const Text('+2'),
             ),
             const SizedBox(height: 10,),
               FloatingActionButton(
               heroTag: '3',
-            onPressed: (){},
+            onPressed: (){
+              increaseCounterBy(context);
+            },
             child: const Text('+1'),
             ),
             const SizedBox(height: 10,),
-
+    
         ],
       ),
     );
